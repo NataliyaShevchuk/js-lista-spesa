3fd8e847b30d2e540b8fe02dd7a0c6b0e4f47f72
NataliyaShevchuk <nata.shevchuk@icloud.com>
2022-11-08 15:54:29 +0100

    aggiornamento

--- js/main.js
+++ js/main.js
@@ -1,15 +1,38 @@
+// Descrizione:
+// Rivediamo l’esercizio visto insieme, ma questa volta con il ciclo while al posto del ciclo for. 
+// Facciamo attenzione a non dimenticare tutte le differenze implementative che il ciclo while ci impone,
+//  in particolare alla variabile di indice.
+// Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) 
+// gli elementi della lista individualmente con un ciclo while.
+
+
 const listaSpesa = [ "pasta", "sugo", "parmiggiano", "tonno"];
 const ingredienteEl = document.getElementById("ingrediente");
 
+let ingrediente = false;
 let i = 0;
 
 
-while (i < listaSpesa.length) { 
+while ( ingrediente === false && i < listaSpesa.length) { 
     ingredienteEl.innerHTML = (listaSpesa);
 
+    if (listaSpesa[i] === ingrediente ){
+        ingrediente === true;
+        const risultato = ingrediente;
+        console.log = (risultato);
+    } 
+
     console.log(listaSpesa[i]);
+    
 
     i++;
 }
+
+if ( ingrediente === true ){
+    ingredienteEl.innerHTML = ( risultato);
+} else{
+    ingredienteEl.innerHTML = ( listaSpesa);
+}
+
 
 
